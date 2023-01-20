4af9464e68bdbf499bd7bf3b64c331faa2e520a8
Christof <chrissi.schwarzenberger@gmx.de>
2023-01-20 22:36:24 +0100

    burgermenu touchstart mobile

--- app.js
+++ app.js
@@ -12,65 +12,11 @@ hamburger.addEventListener('click', () => {
     mobile_menu.classList.toggle('active');
 });
 
-
-/*
-var onScroll = function () {
-  var scroll_position = window.scrollY;
-  if(!document.URL.includes("index")){
-    return;
-  }
-  if(scroll_position > 250){
-    header.style.backgroundColor = "#303030";
-  }else{
-    header.style.backgroundColor = "transparent";
-  }
-}
-
-document.addEventListener("touchmove", onScroll);
-document.addEventListener('scroll', onScroll);
-
-
-document.addEventListener('touchmove', () =>{
-  if(!document.URL.includes("index")){
-      return;
-  }
-  var scroll_position = window.pageYOffset;
-  if(scroll_position > 250){
-      header.style.backgroundColor = "#303030";
-  }else{
-      header.style.backgroundColor = "transparent";
-  }
-});*/
-
-let lastKnownScrollPosition = 0;
-let ticking = false;
-
-function doSomething(scrollPos) {
-  if(!document.URL.includes("index")){
-    return;
-  }
-
-  if(lastKnownScrollPosition > 250){
-    header.style.backgroundColor = "#303030";
-  }else{
-    header.style.backgroundColor = "transparent";
-  }
-}
-
-document.addEventListener("scroll", (event) => {
-  lastKnownScrollPosition = window.pageYOffset;
-
-  if (!ticking) {
-    window.requestAnimationFrame(() => {
-      doSomething(lastKnownScrollPosition);
-      ticking = false;
-    });
-
-    ticking = true;
-  }
+hamburger.addEventListener('touchstart', () => {
+  hamburger.classList.toggle('active');
+  mobile_menu.classList.toggle('active');
 });
 
-/*
 document.addEventListener('scroll', () =>{
     if(!document.URL.includes("index")){
         return;
@@ -82,7 +28,6 @@ document.addEventListener('scroll', () =>{
         header.style.backgroundColor = "transparent";
     }
 });
-*/
 
 menu_item.forEach((item) => {
     item.addEventListener('click', () => {
