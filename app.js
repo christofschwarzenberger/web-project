1aabf3d0d94edf4fa800def18b98e588f085b332
Christof <chrissi.schwarzenberger@gmx.de>
2023-01-20 22:46:29 +0100

    navbar

--- app.js
+++ app.js
@@ -17,16 +17,15 @@ hamburger.addEventListener('touchstart', () => {
   mobile_menu.classList.toggle('active');
 });
 
+
+
 document.addEventListener('scroll', () =>{
-    if(!document.URL.includes("index")){
-        return;
-    }
-    var scroll_position = window.scrollY;
-    if(scroll_position > 250){
-        header.style.backgroundColor = "#303030";
-    }else{
-        header.style.backgroundColor = "transparent";
-    }
+  var scroll_position = window.scrollY;
+  if(scroll_position > 250){
+      header.style.backgroundColor = "#303030dd";
+  }else{
+      header.style.backgroundColor = "transparent";
+  }
 });
 
 menu_item.forEach((item) => {
